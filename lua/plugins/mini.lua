964f8b9af16f2e440808c377636f05a660e13a9a
x004123 <dimitri.kleyn@fv.nrw.de>
2024-03-18 19:14:35 +0100

    mapping surround prev next item.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -9,8 +9,21 @@ return {
         require("mini.tabline").setup({})
         require("mini.comment").setup({})
         require("mini.pairs").setup({})
-        require("mini.surround").setup({})
-        require('mini.indentscope').setup({symbol = '┊', delay = 250,})
+        require("mini.surround").setup({
+            mappings = {
+                add = 'sa',            -- Add surrounding in Normal and Visual modes
+                delete = 'sd',         -- Delete surrounding
+                find = 'sf',           -- Find surrounding (to the right)
+                find_left = 'sF',      -- Find surrounding (to the left)
+                highlight = 'sh',      -- Highlight surrounding
+                replace = 'sr',        -- Replace surrounding
+                update_n_lines = 'sn', -- Update `n_lines`
+                suffix_last = 'p',     -- Suffix to search with "prev" method
+                suffix_next = 'n',     -- Suffix to search with "next" method
+            },
+            search_method = 'cover_or_next',
+        })
+        require('mini.indentscope').setup({ symbol = '┊', delay = 250, })
 
         require("mini.move").setup({
             mappings = {
