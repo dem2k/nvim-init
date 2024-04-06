1e833bdcc87c028d68007a0e444942e2197bbbcf
Dimitri Kleyn <dimitri.kleyn@cdi-ag.de>
2024-04-07 01:48:41 +0200

    incrase yank highlight

--- init.lua
+++ init.lua
@@ -56,7 +56,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 --  desc = 'Highlight when yanking (copying) text',
 --  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank { timeout = 300 }
+    vim.highlight.on_yank { timeout = 500 }
   end,
 })
 
