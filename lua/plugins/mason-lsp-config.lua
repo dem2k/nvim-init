7693025b20d638cb7d4c7b7dcb71fd296a9f94d0
Dimitri Kleyn <dimitri.kleyn@cdi-ag.de>
2024-04-07 01:50:21 +0200

    find launcher jar with *-pattern

--- lua/plugins/mason-lsp-config.lua
+++ lua/plugins/mason-lsp-config.lua
@@ -48,7 +48,7 @@ return {
                 keymap("<leader>lf", ":lua vim.lsp.buf.format({})<cr>",  "Format current Buffer" )
                 keymap("<leader>ld", require("telescope.builtin").lsp_document_symbols, "Document Symbols")
                 keymap("<leader>lw", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace Symbols")
-                vim.keymap.set({ "n", "v" }, "<leader>la", ":lua vim.lsp.buf.code_action()<cr>", { buffer = event.buf, desc = "LSP: Code Action..." })
+                vim.keymap.set({ "n", "v" }, "<leader>ll", ":lua vim.lsp.buf.code_action()<cr>", { buffer = event.buf, desc = "LSP: Code Action..." })
             end
         })
 
@@ -113,8 +113,7 @@ return {
         -- java, last version of jdtls supports java 11 should be 1.12.0. how to download and install this particular verstion with mason? goddamn!
         local jdtls_home = vim.env.JDTLS_HOME or (vim.fn.stdpath("data") .. "/mason/packages/jdtls")
         local jdtls_config = jdtls_home .. "/config_win"
-        local jdtls_launcher = jdtls_home .. "/plugins/"
-            .. (vim.env.JDTLS_LAUNCHER or "org.eclipse.equinox.launcher_1.6.700.v20231214-2017.jar")
+        local jdtls_launcher = jdtls_home .. "/plugins/org.eclipse.equinox.launcher_*.jar"
         lspconfig.jdtls.setup {
             cmd = {
                 --
@@ -137,7 +136,7 @@ return {
                 --
                 "-javaagent:" .. jdtls_home .. "/lombok.jar",
                 --
-                "-jar", jdtls_launcher,
+                "-jar", vim.fn.glob(jdtls_launcher),
                 "-configuration", jdtls_config,
                 -- "-data", (vim.fs.dirname(vim.fs.find({"pom.xml"}, { upward = true })[1]) or vim.fn.getcwd()) .. "/.jdtls"
                 "-data", vim.env.temp .. "/.jdtls"
