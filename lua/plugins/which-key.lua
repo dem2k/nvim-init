32242b3ef42f38972346a334bbec5008d04a067b
x004123 <dimitri.kleyn@fv.nrw.de>
2024-07-17 12:59:48 +0200

    changes for which-key 3.0

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,18 +1,26 @@
 return {
     "folke/which-key.nvim",
-    opts = {},
+    dependencies = {
+        "echasnovski/mini.icons", "nvim-tree/nvim-web-devicons"
+    },
+    event = "VeryLazy",
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    },
     config = function()
-        require("which-key").setup({
-            require("which-key").register({
-                ["<leader>ö"] = { name = "My Commands..." },
-                -- ["<leader>ös"] = { name = "Save Buffer..." },
-                ["<leader>f"] = { name = "Telescope..." },
-                ["<leader>s"] = { name = "Save Buffer..." },
-                ["<leader>b"] = { name = "Buffer..." },
-                ["<leader>g"] = { name = "Git..." },
-                ["<leader>l"] = { name = "Language server..." },
-                ["<leader>t"] = { name = "Terminal..." },
-            })
+        local wk = require("which-key")
+        -- wk.setup({})
+        wk.add({
+            { "<leader>ö", group = "My custom Commands..." },
+            -- ["<leader>ös"] = { name = "Save Buffer..." },
+            { "<leader>f", group = "Telescope..." },
+            { "<leader>s", group = "Save Buffer..." },
+            { "<leader>b", group = "Buffer..." },
+            { "<leader>g", group = "Git..." },
+            { "<leader>l", group = "Language server..." },
+            { "<leader>t", group = "Terminal..." },
         })
     end,
 }
